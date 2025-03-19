5d13bae64aa2b69a8b2fc72a59618452d3e061a7
Yeamin Mahmud <59418750+mahmudresin@users.noreply.github.com>
2025-03-19 11:29:06 +0600

    Update __manifest__.py

--- calculator_app/__manifest__.py
+++ calculator_app/__manifest__.py
@@ -28,6 +28,9 @@
             'calculator_app/static/src/xml/templates.xml',
         ],
     },
+    'images': [
+      'static/description/cover.gif',
+  ],
     'license': 'LGPL-3',
     'application': True,
     'installable': True,
